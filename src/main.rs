d3838a977d4b5c8b9098fd9dacaad8f795ad5d2d
horizontalpie <horizontalpie@gmail.com>
2024-11-05 10:25:32 -0800

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // run("127.0.0.1:0")?.await
     let listener = TcpListener::bind("127.0.0.1:0")?;
     run(listener)?.await
 }
